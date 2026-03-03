35483c4040cebf1f360ce6932f76e9c9ded06e65
cjearanas <cjearanas@pcu.edu.ph>
2026-03-03 21:42:35 +0800

    Initial commit

---
+++ Basic Calcu/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Calcu
+{
+    class Calculator
+    {
+        static void Main()
+        {
+            Console.WriteLine("=== Basic Calculator ===");
+
+            Console.Write("Enter first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter operator (+, -, *, /): ");
+            char op = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+
+                case '-':
+                    result = num1 - num2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+
+                case '*':
+                    result = num1 * num2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+
+                case '/':
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine("Result: " + result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Cannot divide by zero.");
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid operator.");
+                    break;
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
